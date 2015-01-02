bcfcf2c34f678819bb7b064f4a0dc09582301303
antiX-Dave <david@daveserver.info>
2015-01-02 00:02:04 -0500

    Adding some new lib files to try to allow better "sharing" among programs, separate dependance of programs, and start out (again? restart? continue?... :/) with the a new installer.

---
+++ usr/local/lib/desktop-antix/desktop-window-managers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Icon Managers
+icon_managers=rox,space
+
+#Rox Enabled Window Managers
+#Any Window Manager that you know works with rox pinboard add to this 
+#list
+rox_enabled=fluxbox,icewm,jwm,openbox
+
+#Space Enabled Window Managers 
+#Any Window Manager that you know works with spacefm --desktop add to 
+#this list
+space_enabled=fluxbox,icewm,jwm,openbox
+
+#Minimal Window Managers
+#Any window manager that you would like to run just the window manager 
+#add to this list
+min_enabled=fluxbox,icewm,jwm,openbox,wmii
+
+#XDG Autostart Supporting
+#Any window manager that needs (or does not have) xdg directory / 
+#autostart support add to this list
+xdg_support=fluxbox,icewm,jwm,openbox
+
